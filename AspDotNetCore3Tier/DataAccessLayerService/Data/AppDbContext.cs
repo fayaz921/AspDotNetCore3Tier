6af0074dea8706d73ee3e93a42d2d6aa8f861022
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-28 09:12:09 +0500

    Create AppDbContext.cs

---
+++ AspDotNetCore3Tier/DataAccessLayerService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayerService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerService.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Teacher> teachers {  get; set; }
+    }
+}
