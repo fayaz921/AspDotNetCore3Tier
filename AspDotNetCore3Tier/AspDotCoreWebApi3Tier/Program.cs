804b53aad284d11cd0af7b201aadeaea0ff4376c
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-28 10:04:53 +0500

    Update Program.cs

--- AspDotNetCore3Tier/AspDotCoreWebApi3Tier/Program.cs
+++ AspDotNetCore3Tier/AspDotCoreWebApi3Tier/Program.cs
@@ -1,16 +1,24 @@
+using DataAccessLayerService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.   
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Add Db
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConn")));
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
     app.MapOpenApi();
 }
 
