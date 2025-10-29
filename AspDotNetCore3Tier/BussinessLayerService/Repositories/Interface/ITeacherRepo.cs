2e70f91ab130fcf2108e179c2aaac10368809a44
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:19:12 +0500

    Create ITeacherRepo.cs

---
+++ AspDotNetCore3Tier/BussinessLayerService/Repositories/Interface/ITeacherRepo.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayerService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.Repositories.Interface
+{
+    public interface ITeacherRepo
+    {
+        Task SaveTeacher(Teacher teacher);
+    }
+}
