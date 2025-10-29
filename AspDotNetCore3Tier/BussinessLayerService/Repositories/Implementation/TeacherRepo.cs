f222debd27981067611bacf5139f63d9032aefcb
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:19:16 +0500

    Create TeacherRepo.cs

---
+++ AspDotNetCore3Tier/BussinessLayerService/Repositories/Implementation/TeacherRepo.cs
@@ -0,0 +1,25 @@
+﻿using BussinessLayerService.Repositories.Interface;
+using DataAccessLayerService.Data;
+using DataAccessLayerService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.Repositories.Implementation
+{
+    public class TeacherRepo : ITeacherRepo
+    {
+        private readonly AppDbContext appDbContext;
+        public TeacherRepo(AppDbContext _appDbContext)
+        {
+            appDbContext = _appDbContext;
+        }
+        public async Task SaveTeacher(Teacher teacher)
+        {
+            await appDbContext.teachers.AddAsync(teacher);
+            await appDbContext.SaveChangesAsync();
+        }
+    }
+}
