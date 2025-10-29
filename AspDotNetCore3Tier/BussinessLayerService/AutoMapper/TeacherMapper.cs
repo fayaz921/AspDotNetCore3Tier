05b3bdcb3f1c2ebf5dd4289a7c6e040c48b7808a
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:41:01 +0500

    Create TeacherMapper.cs

---
+++ AspDotNetCore3Tier/BussinessLayerService/AutoMapper/TeacherMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BussinessLayerService.Dtos;
+using DataAccessLayerService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.AutoMapper
+{
+    public class TeacherMapper : Profile
+    {
+        public TeacherMapper()
+        {
+            CreateMap<Teacher,AddTeacherDto>().ReverseMap();
+        }
+    }
+}
