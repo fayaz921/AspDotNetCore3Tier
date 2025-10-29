2c5f3bb31ec37cfde22c6a8b9d50a7c8619a4327
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:19:09 +0500

    Create TeacherService.cs

---
+++ AspDotNetCore3Tier/BussinessLayerService/Services/Implementation/TeacherService.cs
@@ -0,0 +1,25 @@
+﻿using BussinessLayerService.Repositories.Interface;
+using BussinessLayerService.Services.Interface;
+using DataAccessLayerService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.Services.Implementation
+{
+    internal class TeacherService : ITeacherService
+    {
+        private readonly ITeacherRepo teacherRepo;
+        public TeacherService(ITeacherRepo _teacherrepo)
+        {
+            teacherRepo = _teacherrepo;
+        }
+        public async Task<string> SaveTeacher(Teacher teacher)
+        {
+             await teacherRepo.SaveTeacher(teacher);
+             return "Teacher Saved Successfully";
+        }
+    }
+}
