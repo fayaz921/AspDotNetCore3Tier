d6a09cb7349584eb09aae7cd56d4e6aa6451c450
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:41:25 +0500

    create addteacherdto

---
+++ AspDotNetCore3Tier/BussinessLayerService/Dtos/AddTeacherDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.Dtos
+{
+    public record AddTeacherDto(string Name, string Specialization);
+
+}
