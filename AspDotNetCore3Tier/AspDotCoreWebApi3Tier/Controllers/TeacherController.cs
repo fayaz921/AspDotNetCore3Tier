11461a7af4a9b80ff4a5ef0e010e82a67ea79fd8
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:19:25 +0500

    Create TeacherController.cs

---
+++ AspDotNetCore3Tier/AspDotCoreWebApi3Tier/Controllers/TeacherController.cs
@@ -0,0 +1,25 @@
+﻿using BussinessLayerService.Services.Interface;
+using DataAccessLayerService.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotCoreWebApi3Tier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly ITeacherService teacherService;
+        public TeacherController(ITeacherService _teacherservice)
+        {
+            teacherService = _teacherservice;
+        }
+        [HttpPost]
+        [Route("SaveTeacher")]
+        public async Task<IActionResult> SaveTeacher(Teacher teacher)
+        {
+            await teacherService.SaveTeacher(teacher);
+            return Ok("Teacher Saved Successfully");
+        }
+    }
+}
