031ac4197c27853a1ea00b7498f0c4bc6c53fae4
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-29 15:19:05 +0500

    Create ITeacherService.cs

---
+++ AspDotNetCore3Tier/BussinessLayerService/Services/Interface/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayerService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.Services.Interface
+{
+    public interface ITeacherService
+    {
+        Task<string> SaveTeacher(Teacher teacher);
+    }
+}
