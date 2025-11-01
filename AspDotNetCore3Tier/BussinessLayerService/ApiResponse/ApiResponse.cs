c9362c6730122c15bc1ad3d243f1a77a9d3118ed
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-01 10:17:04 +0500

    Create ApiResponse.cs

---
+++ AspDotNetCore3Tier/BussinessLayerService/ApiResponse/ApiResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayerService.ApiResponse
+{
+    public class ApiResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public string? Error { get; set; }
+
+        public static ApiResponse<T> SuccessResponse(T? data)
+        {
+            return new ApiResponse<T>
+            {
+                Data = data,
+                IsSuccess = true
+            };
+        }
+
+        public static ApiResponse<T> FailureResponse(string? error)
+        {
+            return new ApiResponse<T>
+            {
+                Error = error,
+                IsSuccess = false
+            };
+        }
+    }
+}
